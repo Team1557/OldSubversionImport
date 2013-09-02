51aab1b6832be61d67c97fbff4f0defdd812361d
12voltbolt <12voltbolt@ab3f01bb-964b-4b0c-a84b-46f62308c601>
2013-09-02 22:29:58 +0000

    Added a new NumberModifier, and implemented some old methods

--- trunk/RobotTemplate/src/twelvevoltbolt/robotics/controls/NumberModifier.java
+++ trunk/RobotTemplate/src/twelvevoltbolt/robotics/controls/NumberModifier.java
@@ -1,5 +1,6 @@
 package twelvevoltbolt.robotics.controls;
 
+
 /**
  * An abstract class which, when implemented, allows a number to be passed through it's modify function.
  */
@@ -22,6 +23,31 @@ public abstract class NumberModifier {
     public static NumberModifier SQUARED = new NumberModifier() {
         public double modify(double number) {
             return number * number * (number < 0 ? -1 : 1);
+        }
+    };
+    
+    /**
+     * Gets the average of the last 10 values it's been told to modify.
+     */
+    public static NumberModifier AVERAGE = new NumberModifier() {
+        double[] last = new double[10];
+        public double modify(double number) {
+            double total = 0;
+            
+            // Push the numbers forward by one, and add them to the total.
+            for (int i = 1; i < last.length; i++) {
+                double val = last[i - 1];
+                
+                total += val;
+                last[i] = val;
+            }
+            
+            // Add the lastest value.
+            total += number;
+            last[0] = number;
+            
+            // Return the average.
+            return total / last.length;
         }
     };
 }
