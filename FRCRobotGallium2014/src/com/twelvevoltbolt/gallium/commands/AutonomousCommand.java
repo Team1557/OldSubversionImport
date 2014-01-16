74d9639f620720f7e012bce1dace9c21eb65d4a7
12voltbolt <team1557@gmail.com>
2014-01-16 22:44:45 +0000

    Added basic autonomous to drive forward and fire

--- FRCRobotGallium2014/src/com/twelvevoltbolt/gallium/commands/AutonomousCommand.java
+++ FRCRobotGallium2014/src/com/twelvevoltbolt/gallium/commands/AutonomousCommand.java
@@ -1,36 +1,19 @@
 
 package com.twelvevoltbolt.gallium.commands;
 
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
+
 /**
  *
  * @author bradmiller
  */
-public class AutonomousCommand extends CommandBase {
+public class AutonomousCommand extends CommandGroup {
 
     public AutonomousCommand() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+        addSequential(new DriveForwardCommand(0.5, 2));
+        addSequential(new WaitCommand(1));
+        addSequential(new DirectionalFireCommand(0));
     }
-
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
-
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    }
-
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
-
-    // Called once after isFinished returns true
-    protected void end() {
-    }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+    
 }
