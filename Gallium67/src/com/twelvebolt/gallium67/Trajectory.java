05ea240eee0ee90efc5a94a8c0518ce591f134a4
JacobLambda <jacoblambda@users.noreply.github.com>
2014-01-18 19:39:17 +0000

    lol i dont even know

---
+++ Gallium67/src/com/twelvebolt/gallium67/Trajectory.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.twelvebolt.gallium67;
+
+/**
+ *
+ * @author david
+ */
+public class Trajectory {
+   
+    public Trajectory(int mapX, int mapY, byte hotGoal) {
+    
+        public byte mapX = 0;
+        public byte mapY = 0;
+        public byte joystickX = 0;
+        public byte joystickY = 0;
+        public byte hotGoal = 0;
+    
+    }
+}
