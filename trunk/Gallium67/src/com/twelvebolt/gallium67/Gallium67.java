f65877770f32948b628427749994e41c21f55df7
12voltbolt <12voltbolt@ab3f01bb-964b-4b0c-a84b-46f62308c601>
2014-01-18 15:14:37 +0000

    Fixed TableKeyNotFoundException exception.

--- trunk/Gallium67/src/com/twelvebolt/gallium67/Gallium67.java
+++ trunk/Gallium67/src/com/twelvebolt/gallium67/Gallium67.java
@@ -1,40 +1,29 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.twelvebolt.gallium67;
 
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
-/**
- *
- * @author david
- */
 public class Gallium67 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws InterruptedException {
-        // TODO code application logic here
-        Gallium67.run();
-    }
-    
-    public static void run() throws InterruptedException {
         NetworkTable.setClientMode();
-        //NetworkTable.setIPAddress("10.15.57.2");
-        //NetworkTable.setIPAddress("127.0.0.1");
         NetworkTable.setIPAddress("localhost");
         
         NetworkTable table = NetworkTable.getTable("SmartDashboard");
         
-        System.out.println(table);
+        if (!table.containsKey("id")) {
+            table.putNumber("id", 0);
+        }
+        if (!table.containsKey("Hello")) {
+            table.putNumber("Hello", 0);
+        }
         
         int id = (int) table.getNumber("id");
         table.putNumber("id", id + 1);
         table.putNumber("time" + id, 500);
         
-        int i = 0;
         while (true) {
             table.putNumber("Hello", table.getNumber("Hello") + 1);
             Thread.sleep((long) Math.max(table.getNumber("time" + id),1));
