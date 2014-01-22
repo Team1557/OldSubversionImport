7703c0f803098c6b5c73bb01a8ff4c00e784de81
JacobLambda <jacoblambda@users.noreply.github.com>
2014-01-22 01:10:48 +0000

    Fixed autonomous.

---
+++ Gallium67/src/com/twelvebolt/gallium67/calculation/Calculation.java
@@ -0,0 +1,38 @@
+package com.twelvebolt.gallium67.calculation;
+
+public class Calculation {
+    public double mapX;
+    public double mapY;
+    public double goalNearX;
+    public double goalNearY;
+    public double goalMidX;
+    public double goalMidY;
+    public double goalFarX;
+    public double goalFarY;
+    public double joystickX;
+    public double joystickY;
+    public double magnitude;
+    public double fireAngle;
+    public double fireRange;
+    public boolean inRange;
+    
+    public boolean inRange(double mapX, double mapY, boolean goalTarget) {
+        if((Math.sqrt(Math.pow((goalNearX-mapX),2)+Math.pow((goalNearY-mapY),2)) > 5) || (Math.sqrt(Math.pow((goalMidX-mapX),2)+Math.pow((goalMidY-mapY),2)) > 5) || (Math.sqrt(Math.pow((goalFarX-mapX),2)+Math.pow((goalFarY-mapY),2)) > 5)){
+           return inRange = true;
+        }
+        else {
+            return inRange = false;
+        }
+    }
+    
+    public double resolveAngle(double mapX, double mapY,boolean goalTarget) {
+        
+        return fireAngle;
+    }
+    
+    public double Trajectory() {
+        return 0;
+        
+    }
+    
+}
