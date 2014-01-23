f85b3904960df5c81314819bfdf95950f1edee6b
JacobLambda <JacobLambda@ab3f01bb-964b-4b0c-a84b-46f62308c601>
2014-01-23 01:02:01 +0000

    lol i dont even know

--- trunk/Gallium67/src/com/twelvebolt/gallium67/Calculation.java
+++ trunk/Gallium67/src/com/twelvebolt/gallium67/Calculation.java
@@ -3,12 +3,18 @@ package com.twelvebolt.gallium67;
 public class Calculation {
     public double mapX;
     public double mapY;
-    public double goalNearX;
-    public double goalNearY;
-    public double goalMidX;
-    public double goalMidY;
-    public double goalFarX;
-    public double goalFarY;
+    public double goalLeftNearX;
+    public double goalLeftNearY;
+    public double goalLeftMidX;
+    public double goalLeftMidY;
+    public double goalLeftFarX;
+    public double goalLeftFarY;
+    public double goalRightNearX;
+    public double goalRightNearY;
+    public double goalRightMidX;
+    public double goalRightMidY;
+    public double goalRightFarX;
+    public double goalRightFarY;
     public double joystickX;
     public double joystickY;
     public double magnitude;
@@ -17,18 +23,35 @@ public class Calculation {
     public boolean inRange;
     
     public boolean inRange(double mapX, double mapY, boolean goalTarget) {
-        if ((Math.sqrt(Math.pow((goalNearX - mapX), 2) + Math.pow((goalNearY - mapY), 2)) > 5) || (Math.sqrt(Math.pow((goalMidX - mapX), 2) + Math.pow((goalMidY - mapY), 2)) > 5) || (Math.sqrt(Math.pow((goalFarX - mapX), 2) + Math.pow((goalFarY - mapY), 2)) > 5)) {
-            return inRange = true;
+        if (goalTarget == true) {
+            if ((Math.sqrt(Math.pow((goalRightNearX - mapX), 2) + Math.pow((goalRightNearY - mapY), 2)) > fireRange)
+                    || (Math.sqrt(Math.pow((goalRightMidX - mapX), 2) + Math.pow((goalRightMidY - mapY), 2)) > fireRange)
+                    || (Math.sqrt(Math.pow((goalRightFarX - mapX), 2) + Math.pow((goalRightFarY - mapY), 2)) > fireRange)) {
+                return inRange = true;
+            } else {
+                return inRange = false;
+            }
         } else {
-            return inRange = false;
+
+            if ((Math.sqrt(Math.pow((goalLeftNearX - mapX), 2) + Math.pow((goalLeftNearY - mapY), 2)) > fireRange)
+                    || (Math.sqrt(Math.pow((goalLeftMidX - mapX), 2) + Math.pow((goalLeftMidY - mapY), 2)) > fireRange)
+                    || (Math.sqrt(Math.pow((goalLeftFarX - mapX), 2) + Math.pow((goalLeftFarY - mapY), 2)) > fireRange)) {
+                return inRange = true;
+            } else {
+                return inRange = false;
+            }
         }
     }
 
-    public double resolveAngle(double mapX, double mapY,boolean goalTarget) {      
-        return fireAngle;
+    public double resolveAngle(double mapX, double mapY,boolean goalTarget) {
+        if (goalTarget == true) {
+            1
+        } else {
+            1
+        }
     }
     
-    public double trajectory() {
+    public double trajectory() {        
         return 0;        
     }    
 }
