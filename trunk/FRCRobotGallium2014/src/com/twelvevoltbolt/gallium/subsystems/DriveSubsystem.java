2435890c9fa54c900c46b4a26f6e980ec10ff9b3
JacobLambda <JacobLambda@ab3f01bb-964b-4b0c-a84b-46f62308c601>
2014-01-29 00:32:07 +0000

    derp in code(typo)

--- trunk/FRCRobotGallium2014/src/com/twelvevoltbolt/gallium/subsystems/DriveSubsystem.java
+++ trunk/FRCRobotGallium2014/src/com/twelvevoltbolt/gallium/subsystems/DriveSubsystem.java
@@ -66,7 +66,7 @@ public class DriveSubsystem extends Subsystem {
     }
     
     public boolean getGear() {
-        return this.reversed;
+        return this.gear;
     }
     
     public void toggleGear() {
