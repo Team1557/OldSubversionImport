6fb5fbff0b6d2d331a09e3647f0410be30d8f5ff
12voltbolt <12voltbolt@ab3f01bb-964b-4b0c-a84b-46f62308c601>
2014-02-10 03:00:55 +0000

    Added support for robot position to AutonomousConfig.

--- trunk/AutonomousConfig/src/com/twelvevoltbolt/autonomous/Main.java
+++ trunk/AutonomousConfig/src/com/twelvevoltbolt/autonomous/Main.java
@@ -46,6 +46,8 @@ public class Main extends javax.swing.JFrame {
         debugBox = new javax.swing.JCheckBox();
         roborealmAimBox = new javax.swing.JCheckBox();
         waitForHotGoalBox = new javax.swing.JCheckBox();
+        jLabel2 = new javax.swing.JLabel();
+        directionBox = new javax.swing.JComboBox();
         jPanel2 = new javax.swing.JPanel();
         driveTimeLabel = new javax.swing.JLabel();
         driveTimeAfterFire = new javax.swing.JSlider();
@@ -108,6 +110,15 @@ public class Main extends javax.swing.JFrame {
             }
         });
 
+        jLabel2.setText("Location:");
+
+        directionBox.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Left", "Middle", "Right", "Goalie Zone" }));
+        directionBox.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                directionBoxActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -115,29 +126,40 @@ public class Main extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(roborealmAimBox, javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(driveForwardBox)
-                            .addComponent(fireBallBox)
-                            .addComponent(waitForHotGoalBox)
-                            .addComponent(debugBox))
-                        .addGap(26, 26, 26)))
-                .addContainerGap(9, Short.MAX_VALUE))
+                            .addComponent(jLabel2)
+                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(roborealmAimBox, javax.swing.GroupLayout.Alignment.TRAILING)
+                                .addGroup(jPanel1Layout.createSequentialGroup()
+                                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addComponent(driveForwardBox)
+                                        .addComponent(fireBallBox))
+                                    .addGap(42, 42, 42)))
+                            .addComponent(debugBox)
+                            .addComponent(waitForHotGoalBox)))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(directionBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
                 .addComponent(fireBallBox)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(driveForwardBox)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(roborealmAimBox)
-                .addGap(1, 1, 1)
-                .addComponent(waitForHotGoalBox)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(waitForHotGoalBox)
+                .addGap(1, 1, 1)
                 .addComponent(debugBox)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(directionBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
 
@@ -214,7 +236,7 @@ public class Main extends javax.swing.JFrame {
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addGap(25, 25, 25)
                         .addComponent(jLabel3)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 31, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 20, Short.MAX_VALUE)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -295,16 +317,14 @@ public class Main extends javax.swing.JFrame {
                 .addComponent(driveTime1Slider, javax.swing.GroupLayout.PREFERRED_SIZE, 0, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap(16, Short.MAX_VALUE))
+                    .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(33, 33, 33)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(lockValues, javax.swing.GroupLayout.DEFAULT_SIZE, 128, Short.MAX_VALUE)
-                            .addComponent(resetButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                        .addGap(37, 37, 37))))
+                            .addComponent(resetButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(lockValues, javax.swing.GroupLayout.PREFERRED_SIZE, 128, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(16, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -312,20 +332,21 @@ public class Main extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(214, 214, 214)
-                        .addComponent(driveTime1Slider, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(driveTime1Slider, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
                                 .addContainerGap()
                                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(layout.createSequentialGroup()
-                                .addGap(49, 49, 49)
+                                .addGap(64, 64, 64)
                                 .addComponent(lockValues)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(resetButton)))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(16, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
         pack();
@@ -409,6 +430,7 @@ public class Main extends javax.swing.JFrame {
 
     private void lockValuesStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_lockValuesStateChanged
         if (lockValues.isSelected()) {
+            resetButton.setEnabled(false);
             fireBallBox.setEnabled(false);
             driveForwardBox.setEnabled(false);
             roborealmAimBox.setEnabled(false);
@@ -418,7 +440,9 @@ public class Main extends javax.swing.JFrame {
             driveTimeAfterFire.setEnabled(false);
             driveSpeed.setEnabled(false);
             driveSpeedAfterFire.setEnabled(false);
+            directionBox.setEnabled(false);
         } else {
+            resetButton.setEnabled(true);
             fireBallBox.setEnabled(true);
             driveForwardBox.setEnabled(true);
             roborealmAimBox.setEnabled(true);
@@ -428,22 +452,38 @@ public class Main extends javax.swing.JFrame {
             driveTimeAfterFire.setEnabled(true);
             driveSpeed.setEnabled(true);
             driveSpeedAfterFire.setEnabled(true);
+            directionBox.setEnabled(true);
         }
     }//GEN-LAST:event_lockValuesStateChanged
 
     private void resetButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_resetButtonActionPerformed
-        if (!lockValues.isSelected()) {
-            fireBallBox.setSelected(true);
-            driveForwardBox.setSelected(true);
-            roborealmAimBox.setSelected(true);
-            waitForHotGoalBox.setSelected(true);
-            debugBox.setSelected(false);
-            driveTime.setValue(100);
-            driveTimeAfterFire.setValue(100);
-            driveSpeed.setValue(100);
-            driveSpeedAfterFire.setValue(100);
-        }
+        fireBallBox.setSelected(true);
+        driveForwardBox.setSelected(true);
+        roborealmAimBox.setSelected(true);
+        waitForHotGoalBox.setSelected(true);
+        debugBox.setSelected(false);
+        driveTime.setValue(100);
+        driveTimeAfterFire.setValue(100);
+        driveSpeed.setValue(100);
+        driveSpeedAfterFire.setValue(100);
     }//GEN-LAST:event_resetButtonActionPerformed
+
+    private void directionBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_directionBoxActionPerformed
+        try {
+            Object selected = directionBox.getSelectedItem();
+            if (selected.equals("Left")) {
+                AutonomousTables.setAutoString("DIRECTION", "LEFT");
+            } else if (selected.equals("Middle")) {
+                AutonomousTables.setAutoString("DIRECTION", "MIDDLE");
+            } else if (selected.equals("Right")) {
+                AutonomousTables.setAutoString("DIRECTION", "RIGHT");
+            } else {
+                AutonomousTables.setAutoString("DIRECTION", "GOALIE");
+            }           
+        } catch (NotConnectedException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_directionBoxActionPerformed
 
     /**
      * @param args the command line arguments
@@ -471,6 +511,7 @@ public class Main extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.ButtonGroup buttonGroup1;
     private javax.swing.JCheckBox debugBox;
+    private javax.swing.JComboBox directionBox;
     private javax.swing.JCheckBox driveForwardBox;
     private javax.swing.JSlider driveSpeed;
     private javax.swing.JSlider driveSpeedAfterFire;
@@ -486,6 +527,7 @@ public class Main extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
