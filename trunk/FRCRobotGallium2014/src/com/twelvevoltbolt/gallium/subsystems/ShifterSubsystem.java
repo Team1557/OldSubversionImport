676b4097f47c20c4456afdb3733df34c931e2f53
12voltbolt <12voltbolt@ab3f01bb-964b-4b0c-a84b-46f62308c601>
2014-02-19 00:04:08 +0000

    Added encoder class, then scrapped

--- trunk/FRCRobotGallium2014/src/com/twelvevoltbolt/gallium/subsystems/ShifterSubsystem.java
+++ trunk/FRCRobotGallium2014/src/com/twelvevoltbolt/gallium/subsystems/ShifterSubsystem.java
@@ -6,6 +6,7 @@
 package com.twelvevoltbolt.gallium.subsystems;
 
 import com.twelvevoltbolt.gallium.RobotMap;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
@@ -16,9 +17,17 @@ public class ShifterSubsystem extends Subsystem {
     Solenoid superShifter1;
     Solenoid superShifter2;
     
+//    private Encoder encoder;
+    
     public ShifterSubsystem() {
         superShifter1 = new Solenoid(RobotMap.superShifter1);
         superShifter2 = new Solenoid(RobotMap.superShifter2);
+        
+//        encoder = new Encoder(RobotMap.encoderA, RobotMap.encoderB, false, CounterBase.EncodingType.k4X);
+//        encoder.setDistancePerPulse(1);
+//        encoder.setMaxPeriod(10);
+//        encoder.setMinRate(1);
+//        encoder.start();
         
         LiveWindow.addActuator(getName(), "SuperShifter on", superShifter1);
         LiveWindow.addActuator(getName(), "SuperShifter off", superShifter2);
